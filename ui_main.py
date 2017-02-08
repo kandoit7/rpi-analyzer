ecf0388cc08555666dbfeefb36c42fc582c402fe
kandoit7 <jmin8884@gmail.com>
2017-02-08 11:56:44 +0900

    Add files via upload

--- ui_main.py
+++ ui_main.py
@@ -25,7 +25,7 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(460, 300)
+        MainWindow.resize(460, 279)
         MainWindow.setStyleSheet(_fromUtf8("QMainWinodw{\n"
 "background-color: rgb(0, 0, 0);\n"
 "}\n"
@@ -63,22 +63,19 @@ class Ui_MainWindow(object):
 "}"))
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.MIC1 = QtGui.QPushButton(self.centralwidget)
-        self.MIC1.setGeometry(QtCore.QRect(363, 5, 91, 20))
-        self.MIC1.setStyleSheet(_fromUtf8(""))
-        self.MIC1.setObjectName(_fromUtf8("MIC1"))
-        self.PCM = PlotWidget(self.centralwidget)
-        self.PCM.setGeometry(QtCore.QRect(5, 175, 451, 121))
-        self.PCM.setObjectName(_fromUtf8("PCM"))
+        self.EXIT = QtGui.QPushButton(self.centralwidget)
+        self.EXIT.setGeometry(QtCore.QRect(363, 5, 91, 20))
+        self.EXIT.setStyleSheet(_fromUtf8(""))
+        self.EXIT.setObjectName(_fromUtf8("EXIT"))
         self.FFT = PlotWidget(self.centralwidget)
-        self.FFT.setGeometry(QtCore.QRect(5, 29, 451, 121))
+        self.FFT.setGeometry(QtCore.QRect(5, 152, 451, 121))
         self.FFT.setObjectName(_fromUtf8("FFT"))
+        self.PCM = PlotWidget(self.centralwidget)
+        self.PCM.setGeometry(QtCore.QRect(5, 29, 451, 121))
+        self.PCM.setObjectName(_fromUtf8("PCM"))
         self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(5, 5, 100, 20))
+        self.label.setGeometry(QtCore.QRect(5, 5, 351, 20))
         self.label.setObjectName(_fromUtf8("label"))
-        self.label_2 = QtGui.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(5, 152, 100, 20))
-        self.label_2.setObjectName(_fromUtf8("label_2"))
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -86,8 +83,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.MIC1.setText(_translate("MainWindow", "EXIT", None))
-        self.label.setText(_translate("MainWindow", "PCM Data", None))
-        self.label_2.setText(_translate("MainWindow", "Frequency Data", None))
+        self.EXIT.setText(_translate("MainWindow", "EXIT", None))
+        self.label.setText(_translate("MainWindow", "PCM & Frequency Data", None))
 
 from pyqtgraph import PlotWidget
